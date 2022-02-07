8b470a902a03d13b7efab88919ec4eefc7bbe161
Garrett <garres4@vt.edu>
2022-02-07 13:58:44 -0700

    set up react app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
+    include ActionController::Cookies
+    
     wrap_parameters format: []
+
 end
---
+++ db/migrate/20220207203841_add_recovery_password_digest_to_admin.rb
@@ -0,0 +1,5 @@
+class AddRecoveryPasswordDigestToAdmin < ActiveRecord::Migration[7.0]
+  def change
+    add_column :admins, :recovery_password_digest, :string
+  end
+end
