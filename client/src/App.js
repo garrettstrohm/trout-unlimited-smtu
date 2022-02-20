dafc8e207bd67d135562a3b7c6ac0eb229eb47fc
Garrett <garres4@vt.edu>
2022-02-20 11:31:00 -0700

    set up login path

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.js'
+import Login from './admin/Login';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path={process.env.REACT_APP_API_KEY} element={<Login />} />
       </Routes>
     </div>
   );
