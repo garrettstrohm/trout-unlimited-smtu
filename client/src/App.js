c05c4e4a4ba066ad3ec9aa1175a7f78cbaaacce3
Garrett <garres4@vt.edu>
2022-02-23 15:48:41 -0700

    starting a rough skeletal mock up and exploring styled components

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.js'
 import Login from './admin/Login';
+import {Container} from './components/styled/Container.styled.js'
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path={process.env.ADMIN_ROUTE} element={<Login />} />
       </Routes>
-    </div>
+    </Container>
   );
 }
 
