dcc029bed6ecf5489240cc5847490e45484a6457
Garrett <garres4@vt.edu>
2022-03-14 09:53:11 -0600

    refactored background image

---
+++ client/src/pages/About/Data.js
@@ -0,0 +1,8 @@
+import flyfishing from '../../assets/flyfishing.jpeg'
+
+export const aboutObjOne = {
+    lightBg: true,
+    bgImg: `url(${flyfishing})`,
+    lightText: true,
+    message: "About Snowy Mountain Trout Unlimited"
+  };
